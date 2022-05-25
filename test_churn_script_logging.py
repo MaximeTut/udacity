fe5018be168fa17271bba2421be851e0a48a8588
MaximeTut <75135024+MaximeTut@users.noreply.github.com>
2022-05-25 16:14:09 +0200

    test

--- test_churn_script_logging.py
+++ test_churn_script_logging.py
@@ -1,23 +1,26 @@
 import os
 import logging
 import pytest
+from pandas.api.types import is_numeric_dtype
 from pathlib import Path
 from churn_library import import_data, perform_eda, encoder_helper, perform_feature_engineering,\
 	classification_report_image, feature_importance_plot, train_models
+PATH  = r"C:\Users\maxim\OneDrive\Bureau\Udacity_MachineLearning_DevOps\Projet_1\data\bank_data.csv"
 
 
-logging.basicConfig(
-    filename='./logs/churn_library.log',
-    level = logging.INFO,
-    filemode='w',
-    format='%(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename=r'C:\Users\maxim\OneDrive\Bureau\Udacity_MachineLearning_DevOps\Projet_1\logs\churn_library.log',
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.INFO)
 
-def test_import():
+
+def test_import_data():
 	'''
 	test data import - this example is completed for you to assist with the other test functions
 	'''
 	try:
-		df = import_data("./data/bank_data.csv")
+		df = import_data(PATH)
 		logging.info("Testing import_data: SUCCESS")
 	except FileNotFoundError as err:
 		logging.error("Testing import_eda: The file wasn't found")
@@ -35,7 +38,7 @@ def test_perform_eda():
 	'''
 	test perform eda function
 	'''
-	path_data = Path("data\bank_data.csv")
+	path_data = Path(PATH)
 	df = import_data(path_data)
 	EDA_PATH = "images\eda"
 
@@ -56,18 +59,27 @@ def test_perform_eda():
 
 
 
-# def test_encoder_helper(encoder_helper):
-# 	pass
-# 	'''
-# 	test encoder helper
-# 	'''
+def test_encoder_helper():
+	'''
+	test encoder helper
+	'''
+	df = import_data(PATH)
+	encoder_helper(df)
+	
+	for x in df.columns:
+		try :
+			assert is_numeric_dtype(df[x])
+			logging.info(f"colonne {x} est bien numerique")
+		except ValueError as err:
+			logging.error(f"La colonne {x} n'est pas au format numérique")
 
 
-# def test_perform_feature_engineering(perform_feature_engineering):
-# 	pass
-# 	'''
-# 	test perform_feature_engineering
-# 	'''
+
+def test_perform_feature_engineering(perform_feature_engineering):
+	pass
+	'''
+	test perform_feature_engineering
+	'''
 
 
 # def test_train_models(train_models):
